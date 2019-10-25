1b20908dcc119e3abe01720890e7012ac07547eb
gabrieltimothy25 <53325851+gabrieltimothy25@users.noreply.github.com>
2019-10-25 16:41:35 +0700

    upload functions exercise

---
+++ exercise-9.js
@@ -0,0 +1,33 @@
+// Soal Satu
+// function shoutout() {
+//   statement = 'Halo Function!'
+//   return statement
+// } 
+
+// console.log(shoutout())
+
+
+// Soal Dua
+// function calculateMultiply(num1, num2) {
+//   hasil = num1 * num2
+//   return hasil
+// }
+// var num1 = 5;
+// var num2 = 6;
+
+// var hasilPerkalian = calculateMultiply(num1,num2);
+// console.log(hasilPerkalian);
+
+
+// Soal Tiga
+// function processSentence(name, age, address, hobby) {
+//   sentence = `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}!`
+//   return sentence
+// }
+// var name = "Agus";
+// var age = 30;
+// var address = "Jln. Malioboro, Yogjakarta";
+// var hobby = "gaming";
+
+// var fullSentence = processSentence(name,age,address,hobby);
+// console.log(fullSentence);
